f9805714dd371bc88be7249a92c458893f64f810
YoneHernandezLeon <yone.hdez.leon@gmail.com>
2026-05-12 12:32:00 +0100

    style: :art: change order of api requests

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -8,16 +8,6 @@ export const getWithdrawals = async () => {
   return response.data;
 };
 
-export const getItems = async () => {
-  const response = await axios.get(`${BASE_URL}items/`);
-  return response.data;
-};
-
-export const getWorkers = async () => {
-  const response = await axios.get(`${BASE_URL}workers/`);
-  return response.data;
-};
-
 export const createWithdrawal = async (data: Withdrawal) => {
   const response = await axios.post(`${BASE_URL}withdrawals/`, data);
   return response.data;
@@ -25,5 +15,15 @@ export const createWithdrawal = async (data: Withdrawal) => {
 
 export const deleteWithdrawalLine = async (id: number) => {
   const response = await axios.delete(`${BASE_URL}withdrawals/${id}/`);
+  return response.data;
+};
+
+export const getItems = async () => {
+  const response = await axios.get(`${BASE_URL}items/`);
+  return response.data;
+};
+
+export const getWorkers = async () => {
+  const response = await axios.get(`${BASE_URL}workers/`);
   return response.data;
 };
