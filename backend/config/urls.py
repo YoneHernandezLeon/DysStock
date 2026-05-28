8deb13caf8db1a04d31d4132bf1e922b3464c76b
YoneHernandezLeon <yone.hdez.leon@gmail.com>
2026-05-28 15:26:49 +0100

    fix: :bug: fix django return-to-main url from admin

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ADMIN_PATH_RETURN_TO_MAIN = os.getenv("ADMIN_RETURN_URL")
 
 urlpatterns = [path("admin/", admin.site.urls), path("api/", include("app.urls"))]
 
-admin.site.site_url = "http://localhost:5173"
+admin.site.site_url = ADMIN_PATH_RETURN_TO_MAIN
